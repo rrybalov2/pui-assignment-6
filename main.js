ae38caf4b32d5ba9113fb661ae795fa087c34f89
rrybalov2 <rrybalov@andrew.cmu.edu>
2017-11-29 00:24:13 -0500

    11.29.17 12:24AM

---
+++ main.js
@@ -0,0 +1,13 @@
+/* Object Constructor*/
+
+/* Starting a JQuery document ready function to make sure the DOM is ready for JS */
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+/* End of doc ready */
+});
+
+/* Initializes AOS Animations Lib */
+  AOS.init({
+    easing: 'ease-in-out-sine'
+  });
